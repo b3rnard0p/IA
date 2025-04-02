86d177336479800577d69a408299e40118c60dad
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2025-04-02 20:52:57 -0300

    Update cromossomo.py

--- Algoritmos Genéticos/Exemplos/BuscaPalavra/cromossomo.py
+++ Algoritmos Genéticos/Exemplos/BuscaPalavra/cromossomo.py
@@ -19,7 +19,7 @@ class Cromossomo:
             if (estado_final[i] in self.palavra):
                nota += 5
                
-            if (i < len(self.palavra) and self.palavra[i] == estado_final[i]):
+            if (i < len(self.palavra) and (self.palavra[i] == estado_final[i])):
                 nota += 50
             
         return nota
@@ -34,29 +34,84 @@ class Cromossomo:
             print(individuo)
 
     @staticmethod
-    def selecionar(populacao, nova_populacao, taxa_selecao):
-        qtd_selecionados = int(len(populacao) * taxa_selecao / 100)
-        nova_populacao.extend(populacao[:qtd_selecionados])
+    def selecionar_por_torneio(populacao, nova_populacao, taxa_selecao):
+        torneio = []
+        qtd_selecionados = taxa_selecao * len(populacao) // 100
+        cromossomo = populacao[0]        
+        nova_populacao.append(cromossomo) 
+        
+        i = 1
+        while i <= qtd_selecionados:
+            c1 = populacao[random.randrange(len(populacao))]
+            
+            while True:            
+                c2 = populacao[random.randrange(len(populacao))]
+                if c2 != c1:
+                    break
+            
+            while True:            
+                c3 = populacao[random.randrange(len(populacao))]
+                if c3 != c2 and c3 != c1:
+                    break            
+
+            torneio.append(c1)
+            torneio.append(c2)
+            torneio.append(c3)
+            torneio.sort(key=lambda x: x.aptidao, reverse=True) 
+
+            selecionado = torneio[0]
+
+            if selecionado not in nova_populacao:
+                nova_populacao.append(selecionado)
+                i += 1
+            
+            torneio.clear()     
+                
 
     @staticmethod
     def reproduzir(populacao, nova_populacao, taxa_reproducao, estado_final):
-        qtd_necessaria = len(populacao) - len(nova_populacao)
-        for _ in range(qtd_necessaria):
-            pai1 = random.choice(populacao[:len(populacao)//2])
-            pai2 = random.choice(populacao[:len(populacao)//2])
+        sPai = sMae = sFilho1 = sFilho2 = ''
+             
+        qtd_reproduzidos = taxa_reproducao * len(populacao) // 100
+
+        i = 0
+        while i < qtd_reproduzidos:            
+            pai = populacao[random.randrange(len(populacao))]
+                
+            while True:            
+                mae = populacao[random.randrange(len(populacao))]
+                if mae != pai:
+                    break               
+
+            sPai = pai.palavra
+            sMae = mae.palavra
             
-            ponto_corte = random.randint(1, len(estado_final)-1)
-            filho_palavra = pai1.palavra[:ponto_corte] + pai2.palavra[ponto_corte:]
-            
-            nova_populacao.append(Cromossomo(filho_palavra, estado_final))
+            sFilho1 = sPai[0 : len(sPai)//2] + sMae[len(sMae)//2 : len(sMae)]
+            sFilho2 = sMae[0 : len(sMae)//2] + sPai[len(sPai)//2 : len(sPai)]
+
+            nova_populacao.append(Cromossomo(sFilho1, estado_final))
+            nova_populacao.append(Cromossomo(sFilho2, estado_final))
+            i = i + 2
+                 
+        while len(nova_populacao) > len(populacao):
+            nova_populacao.pop()
+      
     
     @staticmethod
-    def mutar(nova_populacao, estado_final):
-        for individuo in nova_populacao:
-            if random.random() < 0.1:  
-                idx = random.randint(0, len(estado_final)-1)
-                letra = Util.letras[random.randrange(Util.tamanho)]
-                palavra_lista = list(individuo.palavra)
-                palavra_lista[idx] = letra
-                individuo.palavra = ''.join(palavra_lista)
-                individuo.aptidao = individuo.calcular_aptidao(estado_final)
+    def mutar(populacao, estado_final):
+        qtd_mutantes = random.randrange(len(populacao) // 5)        
+        
+        while qtd_mutantes > 0:
+            posicao_mutante = random.randrange(len(populacao))
+            mutante = populacao[posicao_mutante]
+            print("vai mutar ", mutante)
+            
+            valor_mutado = mutante.palavra
+
+            caracter_mutante = mutante.palavra[random.randrange(len(mutante.palavra))]
+            caracter_sorteado = Util.letras[random.randrange(Util.tamanho)]
+            valor_mutado = valor_mutado.replace(caracter_mutante, caracter_sorteado, 1)          
+            mutante = Cromossomo(valor_mutado, estado_final)
+            
+            populacao[posicao_mutante] = mutante
+            qtd_mutantes -= 1
