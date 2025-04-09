799d717724ae62c586e78ea17a488f04f6109461
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2025-04-09 19:43:25 -0300

    commit

--- Algoritmos Genéticos/Exemplos/BuscaRota/Cidade.py
+++ Algoritmos Genéticos/Exemplos/BuscaRota/Cidade.py
@@ -6,11 +6,15 @@ class Cidade:
         self.aptidao = self.calcular_aptidao()
 
     @staticmethod
-    def gerar_populacao(populacao, tamanho_populacao, num_cidades):
+    def gerar_populacao(tamanho_populacao, num_cidades, permitir_repeticao=False):
+        populacao = []
         for _ in range(tamanho_populacao):
-            rota_gerada = random.sample(range(1, num_cidades+1), num_cidades)
-            individuo = Cidade(rota_gerada)
-            populacao.append(individuo)
+            if permitir_repeticao:
+                rota = [random.randint(1, num_cidades) for _ in range(num_cidades)]
+            else:
+                rota = random.sample(range(1, num_cidades+1), num_cidades)
+            populacao.append(Cidade(rota))
+        return populacao
 
     def calcular_aptidao(self):
         nota = 0
@@ -49,91 +53,94 @@ class Cidade:
             print(f"{i}. {individuo}")
 
     @staticmethod
-    def selecionar_por_torneio(populacao, nova_populacao, taxa_selecao):
-        torneio = []
-        qtd_selecionados = taxa_selecao * len(populacao) // 100
+    def selecionar_por_torneio(populacao, quantidade):
+        selecionados = []
+        while len(selecionados) < quantidade:
+            participantes = random.sample(populacao, 3)
+            vencedor = min(participantes, key=lambda x: x.aptidao)
+            selecionados.append(vencedor)
+        return selecionados
+
+    @staticmethod
+    def reproduzir(populacao, quantidade, num_cidades):
+        filhos = []
+        for _ in range(quantidade // 2):
+            pai, mae = random.sample(populacao[:len(populacao)//2], 2)
+            
+            start, end = sorted(random.sample(range(num_cidades), 2))
+            
+            filho1 = [-1]*num_cidades
+            filho2 = [-1]*num_cidades
+            
+            filho1[start:end+1] = pai.rota[start:end+1]
+            filho2[start:end+1] = mae.rota[start:end+1]
+            
+            current_pos_mae = (end + 1) % num_cidades
+            current_pos_pai = (end + 1) % num_cidades
+            
+            for pos in range(num_cidades):
+                if filho1[pos] == -1:
+                    while mae.rota[current_pos_mae] in filho1:
+                        current_pos_mae = (current_pos_mae + 1) % num_cidades
+                    filho1[pos] = mae.rota[current_pos_mae]
+                    current_pos_mae = (current_pos_mae + 1) % num_cidades
+                    
+                if filho2[pos] == -1:
+                    while pai.rota[current_pos_pai] in filho2:
+                        current_pos_pai = (current_pos_pai + 1) % num_cidades
+                    filho2[pos] = pai.rota[current_pos_pai]
+                    current_pos_pai = (current_pos_pai + 1) % num_cidades
+            
+            filhos.extend([Cidade(filho1), Cidade(filho2)])
         
+        return filhos[:quantidade]
+
+    @staticmethod
+    def aplicar_mutacao(populacao, elite_size, taxa_mutacao, estagnacao_prolongada):
+        taxa_mutacao_ajustada = min(50, taxa_mutacao + estagnacao_prolongada * 5)
+        
+        for individuo in populacao[elite_size:]:
+            if random.random() < taxa_mutacao_ajustada / 100:
+                if random.random() < 0.7:
+                    start, end = sorted(random.sample(range(len(individuo.rota)), 2))
+                    individuo.rota[start:end+1] = reversed(individuo.rota[start:end+1])
+                else:
+                    start, end = sorted(random.sample(range(len(individuo.rota)), 2))
+                    sub_rota = individuo.rota[start:end+1]
+                    random.shuffle(sub_rota)
+                    individuo.rota[start:end+1] = sub_rota
+                
+                individuo.aptidao = individuo.calcular_aptidao()
+        return populacao
+
+    @staticmethod
+    def lidar_com_estagnacao(populacao, tamanho_populacao, num_cidades, historico_aptidao):
+        estagnacao_prolongada = 0
+        
+        if len(historico_aptidao) > 10:
+            historico_aptidao.pop(0)
+            
+            if all(apt == historico_aptidao[-1] for apt in historico_aptidao[-5:]):
+                estagnacao_prolongada += 1
+                print(f"\nEstagnação prolongada nível {estagnacao_prolongada}! Aplicando medidas especiais...")
+                
+                substituicao = max(3, tamanho_populacao // 3)
+                for j in range(1, substituicao + 1):
+                    populacao[-j] = Cidade(random.sample(range(1, num_cidades+1), num_cidades))
+                
+                for individuo in populacao[tamanho_populacao//4:-substituicao]:
+                    start, end = sorted(random.sample(range(len(individuo.rota)), 2))
+                    individuo.rota[start:end+1] = reversed(individuo.rota[start:end+1])
+                    individuo.aptidao = individuo.calcular_aptidao()
+            else:
+                estagnacao_prolongada = max(0, estagnacao_prolongada - 1)
+        
+        return populacao, estagnacao_prolongada
+
+    @staticmethod
+    def reiniciar_populacao_por_estagnacao(populacao, tamanho_populacao, num_cidades):
+        print("\nEstagnação crítica! Reiniciando população...")
         melhor = min(populacao, key=lambda x: x.aptidao)
-        nova_populacao.append(melhor)
-        
-        i = 1
-        while i <= qtd_selecionados:
-            c1 = populacao[random.randrange(len(populacao))]
-            
-            while True:            
-                c2 = populacao[random.randrange(len(populacao))]
-                if c2 != c1:
-                    break
-            
-            while True:            
-                c3 = populacao[random.randrange(len(populacao))]
-                if c3 != c2 and c3 != c1:
-                    break            
-
-            torneio.append(c1)
-            torneio.append(c2)
-            torneio.append(c3)
-            torneio.sort(key=lambda x: x.aptidao) 
-
-            selecionado = torneio[0]
-
-            if selecionado not in nova_populacao:
-                nova_populacao.append(selecionado)
-                i += 1
-            
-            torneio.clear()     
-                
-
-    @staticmethod
-    def reproduzir(populacao, nova_populacao, taxa_reproducao, num_cidades):
-        qtd_reproduzidos = taxa_reproducao * len(populacao) // 100
-
-        i = 0
-        while i < qtd_reproduzidos:            
-            pai = populacao[random.randrange(len(populacao))]
-                
-            while True:            
-                mae = populacao[random.randrange(len(populacao))]
-                if mae != pai:
-                    break               
-
-            ponto1 = random.randint(0, num_cidades - 2)
-            ponto2 = random.randint(ponto1 + 1, num_cidades - 1)
-            
-            def crossover(p1, p2):
-                filho = [-1] * num_cidades
-                filho[ponto1:ponto2+1] = p1[ponto1:ponto2+1]
-                
-                current_pos = (ponto2 + 1) % num_cidades
-                for gene in p2[ponto2+1:] + p2[:ponto2+1]:
-                    if gene not in filho:
-                        filho[current_pos] = gene
-                        current_pos = (current_pos + 1) % num_cidades
-                
-                return filho
-            
-            filho1 = crossover(pai.rota, mae.rota)
-            filho2 = crossover(mae.rota, pai.rota)
-            
-            nova_populacao.append(Cidade(filho1))
-            nova_populacao.append(Cidade(filho2))
-            i = i + 2
-                 
-        while len(nova_populacao) > len(populacao):
-            nova_populacao.pop()
-    
-    @staticmethod
-    def mutar(populacao):
-        populacao.sort(key=lambda x: x.aptidao, reverse=True)
-    
-        qtd_mutantes = max(1, len(populacao) // 3)
-    
-        for i in range(qtd_mutantes):
-            mutante = populacao[i]
-            if len(mutante.rota) >= 2:
-                idx1, idx2 = random.sample(range(len(mutante.rota)), 2)
-                nova_rota = mutante.rota.copy()
-                nova_rota[idx1], nova_rota[idx2] = nova_rota[idx2], nova_rota[idx1]
-                populacao[i] = Cidade(nova_rota)
-    
-        populacao.sort(key=lambda x: x.aptidao)
+        nova_populacao = [melhor]
+        nova_populacao.extend(Cidade.gerar_populacao(tamanho_populacao-1, num_cidades))
+        return nova_populacao
